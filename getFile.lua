8cfdec9f920fa631d27ac428340db39ec6425b6d
Haocen2004 <haocen.minecraft@qq.com>
2020-04-23 16:22:08 +0800

    fix bug

--- getFile.lua
+++ getFile.lua
@@ -1,8 +1,14 @@
-mkdir MinecraftRobotPrograms/Miner
-cd MinecraftRobotPrograms/Miner
--- cc must use http
-wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/AdvancedTurtle.lua
-wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/ChainMine.lua
-wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/EnumFacing.lua
-wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/SpiralMiner.lua
-print("Finish.")
+function getFile.main()
+    mkdir MinecraftRobotPrograms/Miner
+    cd MinecraftRobotPrograms/Miner
+    -- cc must use http
+    wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/AdvancedTurtle.lua
+    wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/ChainMine.lua
+    wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/EnumFacing.lua
+    wget http://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/SpiralMiner.lua
+    print("Finish.")
+end
+
+getFile.main()
+
+return getFile
