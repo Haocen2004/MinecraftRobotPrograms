7daa2a3f0926143f4c9f4a439ae47da385a9de12
Haocen2004 <haocen.minecraft@qq.com>
2020-04-23 16:06:46 +0800

    test auto run

---
+++ getFile.lua
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/AdvancedTurtle.lua
+wget https://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/ChainMine.lua
+wget https://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/EnumFacing.lua
+wget https://raw.githubusercontent.com/Haocen2004/MinecraftRobotPrograms/master/Miner/SpiralMiner.lua
